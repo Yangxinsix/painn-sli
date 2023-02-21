7fc7c695dbfdff491e82bd034477da5133290bb3
xinyang <xinyang@dtu.dk>
2023-02-21 12:23:51 +0100

    readme

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # painn-sli
 This is the source code for paper [Neural Network Potentials for Accelerated Metadynamics of Oxygen Reduction Kinetics at Au-Water Interfaces](10.26434/chemrxiv-2022-b1pt5)
+
+<details open>
+<summary>How to use</summary>
+
+```bash
+pip install -U .
+```
+* See `train.py` in `scripts` for training, and `md_run.py` for running MD simulations by using ASE.
+
+</details>
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="PaiNN",
+    version="1.0.0",
+    description="Library for implementation of message passing neural networks in Pytorch",
+    author="xinyang",
+    author_email="xinyang@dtu.dk",
+    url = "https://github.com/Yangxinsix/PaiNN-model",
+    packages=["PaiNN"],
+    install_requires=[
+        'ase>=3.22.1',
+        'numpy',
+        'asap3>=3.12.8',
+        'torch>=1.10',
+        'scipy',
+)
